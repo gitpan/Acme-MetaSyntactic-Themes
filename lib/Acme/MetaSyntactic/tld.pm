7552da66ea9ce65077ee5da4b83c14b70d081295
Philippe Bruhat (BooK) <book@cpan.org>
2012-07-31 15:14:35 +0000

    Import of BOOK/Acme-MetaSyntactic-Themes-1.012 from CPAN.
    gitpan-cpan-distribution: Acme-MetaSyntactic-Themes
    gitpan-cpan-version:      1.012
    gitpan-cpan-path:         BOOK/Acme-MetaSyntactic-Themes-1.012.tar.gz
    gitpan-cpan-author:       BOOK
    gitpan-cpan-maturity:     released

--- lib/Acme/MetaSyntactic/tld.pm
+++ lib/Acme/MetaSyntactic/tld.pm
@@ -1,13 +1,18 @@
 package Acme::MetaSyntactic::tld;
 use strict;
-use Acme::MetaSyntactic::List;
-our @ISA = qw( Acme::MetaSyntactic::List );
-our $VERSION = '1.000';
+use Acme::MetaSyntactic::MultiList;
+our @ISA = qw( Acme::MetaSyntactic::MultiList );
+our $VERSION = '1.001';
 __PACKAGE__->init();
 
 our %Remote = (
-    source  => 'http://www.ics.uci.edu/pub/websoft/wwwstat/country-codes.txt',
-    extract => sub { return map lc, $_[0] =~ m!^(\S+)!gm },
+    source  => 'http://www.iana.org/domains/root/db/',
+    extract => sub {
+        ( my $type = $_[1]) =~ y/_/-/;
+        local $/;
+        my %type = $_[0] =~ m{<td><a href="/domains/root/db/(\w+).html">.\w+</a></td>\s+<td>([^<]+)</td>\s+<!-- <td>(?:[^<\n]*)}g;
+        return grep $type{$_} eq $type, keys %type;
+    },
 );
 
 1;
@@ -21,7 +26,7 @@ Acme::MetaSyntactic::tld - The Top-Level Domain theme
 The list of top-level domainnames.
 
 The source for the list is
-L<http://www.ics.uci.edu/pub/websoft/wwwstat/country-codes.txt>
+L<http://www.iana.org/domains/root/db/>.
 
 =head1 CONTRIBUTORS
 
@@ -30,6 +35,13 @@ Scott Lanning, Philippe Bruhat (BooK).
 =head1 CHANGES
 
 =over 4
+
+=item *
+
+2012-07-31 - v1.001
+
+Updated the source URL, and updated the data from the source web site
+in Acme-MetaSyntactic version 1.012 (published one day late).
 
 =item *
 
@@ -57,15 +69,23 @@ L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
 =cut
 
 __DATA__
-# names
-ad ae af ag ai al am an ao aq ar as at au aw ax az ba bb bd be bf bg bh
-bi bj bm bn bo br bs bt bv bw by bz ca cc cd cf cg ch ci ck cl cm cn co
-cr cs cu cv cx cy cz de dj dk dm do dz ec ee eg eh er es et fi fj fk fm
-fo fr fx ga gb gd ge gf gh gi gl gm gn gp gq gr gs gt gu gw gy hk hm hn
-hr ht hu id ie il in io iq ir is it jm jo jp ke kg kh ki km kn kp kr kw
-ky kz la lb lc li lk lr ls lt lu lv ly ma mc md mg mh mk ml mm mn mo mp
-mq mr ms mt mu mv mw mx my mz na nc ne nf ng ni nl no np nr nu nz om pa
-pe pf pg ph pk pl pm pn pr ps pt pw py qa re ro ru rw sa sb sc sd se sg sh
-si sj sk sl sm sn so sr st su sv sy sz tc td tf tg th tj tk tl tm tn to tp
-tr tt tv tw tz ua ug uk um us uy uz va vc ve vg vi vn vu wf ws ye yt yu za
-zm zr zw biz com edu gov int mil net org pro aero arpa coop info name nato
+# names country_code
+ac ad ae af ag ai al am an ao aq ar as at au aw ax az ba bb bd be bf bg
+bh bi bj bl bm bn bo bq br bs bt bv bw by bz ca cc cd cf cg ch ci ck cl
+cm cn co cr cu cv cw cx cy cz de dj dk dm do dz ec ee eg eh er es et eu
+fi fj fk fm fo fr ga gb gd ge gf gg gh gi gl gm gn gp gq gr gs gt gu gw
+gy hk hm hn hr ht hu id ie il im in io iq ir is it je jm jo jp ke kg kh
+ki km kn kp kr kw ky kz la lb lc li lk lr ls lt lu lv ly ma mc md me mf
+mg mh mk ml mm mn mo mp mq mr ms mt mu mv mw mx my mz na nc ne nf ng ni
+nl no np nr nu nz om pa pe pf pg ph pk pl pm pn pr ps pt pw py qa re ro
+rs ru rw sa sb sc sd se sg sh si sj sk sl sm sn so sr ss st su sv sx sy
+sz tc td tf tg th tj tk tl tm tn to tp tr tt tv tw tz ua ug uk um us uy
+uz va vc ve vg vi vn vu wf ws ye yt za zm zw
+# names generic
+com info net org
+# names generic_restricted
+biz name pro
+# names infrastructure
+arpa
+# names sponsored
+aero asia cat coop edu gov int jobs mil mobi museum tel travel xxx
